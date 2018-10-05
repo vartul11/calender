62a1c6af0baf1076af0a3fa8ba5f850856c9c1a3
Vartul Mittal <vartulmittal@VARTMITT-LW10341.local>
2018-10-05 17:50:36 +0530

    changes

--- src/client_id.js
+++ src/client_id.js
@@ -1,5 +1,5 @@
 let config = {
-    "client_id": "830380638906-roc479cb9c8mf907mi7esd70vhdb49r9.apps.googleusercontent.com",
+    "client_id": "830380638906-to23losifha6t3meb1f6nvel0cv37jt0.apps.googleusercontent.com",
     "api_key": "AIzaSyBNsZFoNG-uDbSzesqcvFyhnN-4QQ-mvQg",
     "scope": "https://www.googleapis.com/auth/calendar",
     "discoveryDocs": ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
@@ -7,7 +7,7 @@ let config = {
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://www.googleapis.com/oauth2/v3/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_secret": "goWva_lfdmrIH5fN5j65kwHL",
+    "client_secret": "ORCuQVkQKOQVNK4wIZHdvjSv",
     "redirect_uris": ["http://dev.conrati.calender.com"],
     "javascript_origins": ["http://dev.conrati.calender.com"]
 }
